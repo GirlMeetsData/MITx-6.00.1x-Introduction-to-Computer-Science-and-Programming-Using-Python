1fcc9f47f02ec08b0adc1a7a91a61a507d01d73c
Brit <britcava@gmail.com>
2016-09-19 12:11:39 -0700

    Update loan_payment_calc.py

--- loan_payment_calc.py
+++ loan_payment_calc.py
@@ -15,24 +15,21 @@ while month <= 12:
 
 
 # Determining the lowest equal monthly payments needed ot pay off the balance within one year; increments by 10 each time
-month = 0
-balance = 5000
-annualInterestRate = 0.18
-monthlyPaymentRate = 0.02
-
-month = 0
-while month <= 12:
-    if balance != 0:
-        monthlyInterestRate = annualInterestRate/12.0
-        minimumPayment = balance * monthlyPaymentRate
-        unpaidBalance = balance - minimumPayment
-        newBalance = (monthlyInterestRate * unpaidBalance) + unpaidBalance
-    
-    if month == 12:
-        print(round(balance/12,2))
-
-    month += 1 
-    balance = newBalance
-    
+# Paste your code into this box
+monthlyPaymentRate = 10
+monthlyInterestRate = annualInterestRate/12.0
+orig_balance = balance
+while True:
+    month = 0
+    balance = orig_balance #defined
+    for month in range(12):
+        balance -= monthlyPaymentRate        
+        balance = (monthlyInterestRate * balance) +balance
+    if balance <= 0:
+        print('Lowest payment: ' + str(round(monthlyPaymentRate,2)))
+        break
+    else:
+        monthlyPaymentRate += 10
+        
 # Determining the lowest equal monthly payments needed ot pay off the balance within one year; using bisection search
 
