2dedb36d50723c433691c440601edbe5116d85d8
Brit <britcava@gmail.com>
2016-09-27 13:24:40 -0700

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,41 @@
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    # Your Code Here
+
+    return sum(map(len, aDict.values()))
+
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many individual values are in the dictionary.
+    '''
+    result = 0
+    for value in aDict.values():
+        # Since all the values of aDict are lists, aDict.values() will 
+        #  be a list of lists
+        result += len(value)
+    return result
+
+def how_many(aDict):
+    '''
+    Another way to solve the problem.
+
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many individual values are in the dictionary.
+    '''
+    result = 0
+    for key in aDict.keys():
+        result += len(aDict[key])
+    return result
