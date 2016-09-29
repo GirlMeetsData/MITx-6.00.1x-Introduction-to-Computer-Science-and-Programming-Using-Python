19636fa2e31866e876c21d53cce9bd4197684fe9
Brit <britcava@gmail.com>
2016-09-28 19:58:29 -0700

    Update dict.py

--- dict.py
+++ dict.py
@@ -39,3 +39,39 @@ def how_many(aDict):
     for key in aDict.keys():
         result += len(aDict[key])
     return result
+
+def biggest(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: The key with the largest number of values associated with it
+    '''
+    # Your Code Here
+    result = {}
+    for key,value in aDict.items():
+        # Since all the values of aDict are lists, aDict.values() will 
+        #  be a list of lists
+        result[key]=len(value)
+    maxCount = 0
+    maxKey = ''
+    for key,value in result.items():
+        if value > maxCount:
+            maxCount = value
+            maxKey = key
+    return maxKey
+
+#alternative 
+
+def biggest(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: The key with the largest number of values associated with it
+    '''
+    result = None
+    biggestValue = 0
+    for key in aDict.keys():
+        if len(aDict[key]) >= biggestValue:
+            result = key
+            biggestValue = len(aDict[key])
+    return result
