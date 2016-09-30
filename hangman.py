1386519a84e2f1886e7ae7bbe7a417dacbbf7471
Brit <britcava@gmail.com>
2016-09-30 12:30:04 -0700

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,31 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    for letter in secretWord:
+        if letter not in lettersGuessed:
+            return False
+    return True
+
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    # FILL IN YOUR CODE HERE...
+    count = 0
+    answer = ''
+    while count != len(secretWord):
+        for letter in secretWord:
+            if letter not in lettersGuessed:
+                answer += '_ '
+            else:
+                answer += letter
+            count += 1
+        return answer
